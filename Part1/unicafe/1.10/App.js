17261109043d237eef3cfd94d80152f27442613f
David Puerta Martos <dpuerta@usal.es>
2021-11-19 00:29:42 +0100

    full part 1

---
+++ Part1/unicafe/1.10/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+
+const Header = ({header}) => <h1>{header}</h1>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good,neutral,bad }) => {
+  return(
+  <div>
+    <StatisticLine text="good" value ={good} />
+    <StatisticLine text="neutral" value ={neutral} />
+    <StatisticLine text="bad" value ={bad} />
+    <StatisticLine text="Total" value ={good+neutral+bad} />
+    <StatisticLine text="Average" value ={(good-bad)/(good+neutral+bad)} />
+    <StatisticLine text="Positive" value ={good*100/(good+neutral+bad)} />
+  </div>)
+}
+
+const StatisticLine = ({ text, value }) =>{
+  return(
+    <div>
+    {text} {value}  
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+  
+  if (good+neutral+bad === 0) {
+    return(
+      <div>
+        <Header header={'give feedback'} />
+        <Button
+          onClick={increaseGood}
+          text='good'
+        />
+        <Button
+          onClick={increaseNeutral}
+          text='neutral'
+        />     
+        <Button
+          onClick={increaseBad}
+          text='bad'
+        />
+        <Header header={'statistics'} />
+        <div>No feedback given</div>
+        
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header header={'give feedback'} />
+      <Button
+        onClick={increaseGood}
+        text='good'
+      />
+      <Button
+        onClick={increaseNeutral}
+        text='neutral'
+      />     
+      <Button
+        onClick={increaseBad}
+        text='bad'
+      />
+      <Header header={'statistics'} />    
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+    </div>
+  )
+}
+
+export default App
---
+++ Part1/unicafe/1.9/App.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+
+const Header = ({header}) => <h1>{header}</h1>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Display = ({ counter }) => <span>{counter}</span>
+
+const Statistics = ({ good,neutral,bad }) => {
+  return(
+  <div>
+    <div>Total {good+neutral+bad}</div>
+    <div>Average  {(good-bad)/(good+neutral+bad)}</div>
+    <div>Positive {good*100/(good+neutral+bad)}%</div>
+  </div>)
+}
+
+
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+  
+  if (good+neutral+bad === 0) {
+    return(
+      <div>
+        <Header header={'give feedback'} />
+        <Button
+          onClick={increaseGood}
+          text='good'
+        />
+        <Button
+          onClick={increaseNeutral}
+          text='neutral'
+        />     
+        <Button
+          onClick={increaseBad}
+          text='bad'
+        />
+        <Header header={'statistics'} />
+        <div>No feedback given</div>
+        
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header header={'give feedback'} />
+      <Button
+        onClick={increaseGood}
+        text='good'
+      />
+      <Button
+        onClick={increaseNeutral}
+        text='neutral'
+      />     
+      <Button
+        onClick={increaseBad}
+        text='bad'
+      />
+      <Header header={'statistics'} />
+      
+      <div>good <Display counter={good}/></div>
+      <div>neutral <Display counter={neutral}/></div>
+      <div>bad <Display counter={bad}/></div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+    </div>
+  )
+}
+
+export default App
