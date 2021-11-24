a14878d5651e9c56364a0c2cc428054232b7bba5
David Puerta Martos <dpuerta@usal.es>
2021-11-24 21:08:50 +0100

    3.11

---
+++ Part3 106min/3.8 to 3.11/index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('build'))
+
+morgan.token('nname', function(req, res) {  
+    return JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :nname'
+))
+
+app.use(express.json())
+
+
+
+let persons = [
+    {
+      "name": "adssa",
+      "number": "1234-21312",
+      "id": 7
+    },
+    {
+      "name": "David3",
+      "number": "123456",
+      "id": 11
+    },
+    {
+      "name": "david",
+      "number": "1234567",
+      "id": 12
+    }
+  ]
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+    })  
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+    })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
+    })
+
+const generateId = () => {
+    return Math.floor(Math.random()*10000)
+    }
+    
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+        error: 'name or telephone missing' 
+        })
+    }
+    else if (persons.map(person=>person.name.toLowerCase()).includes(body.name.toLowerCase())) {
+        return response.status(400).json({ 
+        error: 'name already added' 
+        })
+    }
+      
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+    })    
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+        `)
+    }) 
+
+app.get('/', function (req, res) {
+    res.send('hello, world!')
+    })    
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
